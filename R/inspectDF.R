85bd5a60cd2a3509708f82c7984f672b28db4bd3
KATO Kohaku <kato.satoshi.0@gmail.com>
2018-07-22 18:02:15 +0900

    add: toEdges

--- R/inspectDF.R
+++ R/inspectDF.R
@@ -1,15 +1,17 @@
-#' parse a decision tree in randomForest into list of path as data.frame
+#' obtain rules as data.frame from arules instead of original inspect().
 #'
-#' @param forest  an object of ensemble trees to be parsed. See \code{\link{randomForest}} or \code{\link{xgboost}}.
-#' @param ktree   an integer. number of decision tree to be parsed. If ktree=NULL (default), all tree will be parsed.
-#' @param resample Logical. If TRUE, trees are ramdomly selected. If FALSE, trees are selected according to head(ktree) from forest.
+#' @param rules  a rule object of  \code{\link{arules}}.
+#' @param sep  string. items in rule string will be separated by sep.
 #'
-#' @return        a list of trees (list).
+#' @return  a data frame of rules.
 #'
 #' @examples
 #' \dontrun{
 #' data(Groceries)
-#' glo.apriori <- apriori(Groceries, parameter = list(confidence=0.01, support=0.01, maxlen=5, minlen=2))
+#'
+#' pars <- list(confidence=0.01, support=0.01, maxlen=5, minlen=2)
+#' glo.apriori <- apriori(Groceries, parameter = pars)
+#'
 #' glo.inspectDF  <- inspectDF(glo.apriori)
 #' glo.inspectDF %>% head
 #' }
@@ -17,21 +19,20 @@
 #' @importFrom magrittr %>%
 #'
 #' @export
-magrittr::`%>%`
 
 inspectDF <- function(rules, sep = ","){
-  stopifnot("rules" %in% class(data))
+  stopifnot("rules" %in% class(rules))
 
-  lhs = arules::labels(arules::lhs(rules), sep, "", "")
-  rhs = arules::labels(arules::rhs(rules), sep, "", "")
+  LHS = arules::labels(arules::lhs(rules), sep, "", "")
+  RHS = arules::labels(arules::rhs(rules), sep, "", "")
   quality = arules::quality(rules)
 
   rules <- cbind(lhs, rhs, quality) %>%
     dplyr::arrange(support, confidence, lift) %>%
-    dplyr::mutate(rule = paste("Rule",row_number()),
-                  LHS = as.character(lhs),
+    dplyr::mutate(rule = paste("Rule", dplyr::row_number()),
+                  LHS = as.character(LHS),
                   RHS = as.character(rhs)) %>%
-    dplyr::select(rule, LHS, RHS, everything()) %>%
+    dplyr::select(rule, LHS, RHS, dplyr::everything()) %>%
     dplyr::select(-lhs, -rhs)
 
   return(rules)
---
+++ R/toEdges.R
@@ -0,0 +1,45 @@
+#' convert rule data.frame to input for graph.data.frame (with from and to colmn).
+#'
+#' @param rules.data.frame  an object of inspectDf, or data.frame with LHS & RHS colmn.
+#' @param sep  string to separate items from rule string.
+#'
+#' @return  a data frame with from and to colmn for graph.data.frame inputs.
+#'
+#' @examples
+#' \dontrun{
+#' data(Groceries)
+#'
+#' pars <- list(confidence=0.01, support=0.01, maxlen=5, minlen=2)
+#' glo.apriori <- apriori(Groceries, parameter = pars)
+#'
+#' glo.inspectDF  <- inspectDF(glo.apriori)
+#' glo.inspectDF %>% head
+#'
+#' #' glo.inspectDF %>% toEdges %>% head
+#' }
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+
+toEdges <- function(rules.data.frame, sep = ","){
+  stopifnot(c("LHS", "RHS") %in% colnames(rules.data.frame))
+  if(NROW(rules.data.frame)<1){
+    return(data.frame(from="NA", to="NA"))
+  }
+
+  rule_rhs_edges <- rules.data.frame %>%
+    dplyr::select(rule, RHS) %>%
+    dplyr::rename(from = rule, to = RHS)
+
+  lhs_rule_edges <- rules.data.frame %>%
+    tidyr::separate_rows(LHS, sep = paste0("\\s*\\",sep,"\\s*")) %>%
+    dplyr::select(LHS, rule) %>%
+    dplyr::rename(from = LHS, to = rule)
+
+  edges.all <- dplyr::bind_rows(lhs_rule_edges, rule_rhs_edges)
+
+  class(edges.all) <- c(class(edges.all), "edges.from.rules")
+  invisible(edges.all)
+}
+
